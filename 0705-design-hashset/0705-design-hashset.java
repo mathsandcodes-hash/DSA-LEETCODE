a271ba421d8f4aff94143d0725c0ea853557fbbf
mathsandcodes-hash <ghoshsoumyapratim@gmail.com>
2026-08-02 23:17:04 +0530

    Time: 11 ms (99.96%), Space: 51.4 MB (85.33%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.java
@@ -0,0 +1,27 @@
+public class MyHashSet {
+    private int[] set;
+
+    public MyHashSet() {
+        // key is in the range [1, 1000000]
+        // 31251 * 32 = 1000032
+        set = new int[31251];
+    }
+
+    public void add(int key) {
+        set[key / 32] |= getMask(key);
+    }
+
+    public void remove(int key) {
+        if (contains(key)) {
+            set[key / 32] ^= getMask(key);
+        }
+    }
+
+    public boolean contains(int key) {
+        return (set[key / 32] & getMask(key)) != 0;
+    }
+
+    private int getMask(int key) {
+        return 1 << (key % 32);
+    }
+}
